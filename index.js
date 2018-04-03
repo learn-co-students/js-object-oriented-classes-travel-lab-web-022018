6a05b8122a7a9a2503a1ecb147b1226b3c5e2320
sinclairstudy <sinclairstudy@gmail.com>
2018-04-03 19:42:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+class Driver {
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(endYear){
+    return endYear - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled(){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    const hor = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
+    const vert = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+    return hor + vert;
+  }
+
+  estimatedTime(peak){
+    if (peak){
+      return this.blocksTravelled() / 2;
+    } else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+
+
+
+
+
+
+}
