7548c3a56161e29a7d6e97d26e540c9632e5866b
dannyb21892 <dannyb21892@verizon.net>
2018-04-04 15:45:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+class Driver {
+  constructor(name, date) {
+    this.name = name
+    this.startDate = new Date(date)
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    let dateyear = new Date(year, 0)
+    let diff = dateyear - this.startDate
+    return Math.ceil(diff/1000/3600/24/365)
+  }
+
+}
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled(){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    let vert = Number(this.endingLocation.vertical.slice(0,2)) - Number(this.beginningLocation.vertical.slice(0,2))
+    let horiz = eastWest.findIndex((input) => this.endingLocation.horizontal === input) - eastWest.findIndex((input) => this.beginningLocation.horizontal === input)
+    return Math.abs(vert) + Math.abs(horiz)
+  }
+
+  estimatedTime(bool){
+    let blocksPerMin
+    if (bool){
+      blocksPerMin = 1/2
+    } else {
+      blocksPerMin = 1/3
+    }
+    return this.blocksTravelled() * blocksPerMin
+  }
+
+}
